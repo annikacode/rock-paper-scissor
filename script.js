73654d0520f3565d4cbd4e51c687d6422a54d90a
Annika <annikahenry5@gmail.com>
2024-01-24 21:02:42 +0200

    added function game to add game rounds

--- script.js
+++ script.js
@@ -39,6 +39,4 @@ function playRound(playerSelection, computerSelection) {
     }
 }
 
-const playerSelection = 'rock';
-const computerSelection = getComputerChoice();
-console.log(playRound(playerSelection, computerSelection));
+
