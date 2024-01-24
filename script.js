2df62b46662843c4e6dafd39d687254f883de98b
Annika <annikahenry5@gmail.com>
2024-01-24 20:31:35 +0200

    changed computerSelection to case insensitive

--- script.js
+++ script.js
@@ -1,44 +1,44 @@
 function getComputerChoice() {
-    // A random number between 0, 1 and 2 is generated
+    // A random number between 0, 1, and 2 is generated
     const randomNum = Math.floor(Math.random() * 3);
 
-    // The random numbers are mapped to either rock, paper or scissors
-
+    // The random numbers are mapped to either rock, paper, or scissors
     switch (randomNum) {
         case 0:
             return 'Rock';
-        case 1: 
+        case 1:
             return 'Paper';
-        case 2: 
-            return 'Scissors'
+        case 2:
+            return 'Scissors';
     }
 }
 
-//Add function to play single round of rock, paper, scissors
+// Add function to play a single round of rock, paper, scissors
 function playRound(playerSelection, computerSelection) {
+    // Selection is made case-insensitive
+    playerSelection = playerSelection.toLowerCase();
+    computerSelection = computerSelection.toLowerCase();
 
-// Selection is made case-insensitive
-playerSelection = playerSelection.toLowerCase();
-
-// Check for a tie 
- if (playerSelection === computerSelection) {
-    return "It's a tie! Try again."
- }
-
- // If the player wins 
-if (
-    (playerSelection === 'rock' && computerSelection === 'scissors') ||
-    (playerSelection === 'paper' && computerSelection === 'rock') ||
-    (playerSelection === 'scissors' && computerSelection === 'paper')
-    ) {
-    return `You win! ${playerSelection} beats ${computerSelection}`;
+    // Check for a tie
+    if (playerSelection === computerSelection) {
+        return "It's a tie! Try again.";
     }
 
-// If player loses 
-return `You Lose! ${computerSelection} beats ${playerSelection}.`;
+    // If the player wins
+    else if (
+        (playerSelection === 'rock' && computerSelection === 'scissors') ||
+        (playerSelection === 'paper' && computerSelection === 'rock') ||
+        (playerSelection === 'scissors' && computerSelection === 'paper')
+    ) {
+        return `You win! ${playerSelection} beats ${computerSelection}`;
+    }
 
+    // If player loses
+    else {
+        return `You lose! ${computerSelection} beats ${playerSelection}.`;
+    }
 }
 
-const playerSelection = 'Rock';
+const playerSelection = 'rock';
 const computerSelection = getComputerChoice();
 console.log(playRound(playerSelection, computerSelection));
