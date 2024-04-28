cbe3a80c6fc0fe118d4e93b8f97827fa9abf3193
Annika <annikahenry5@gmail.com>
2024-04-28 21:36:40 +0200

    Removed function which adds game rounds

--- script.js
+++ script.js
@@ -42,43 +42,5 @@ function playRound(playerSelection, computerSelection) {
     }
 }
 
-// New function to create game rounds until player quits
-function game() {
-    let continuePlaying = true;
 
-    while (continuePlaying) {
-        const playerSelection = prompt("Rock, Paper, Scissor? Type 'quit' to end the game.");
 
-        // Check if the player wants to quit
-        if (playerSelection.toLowerCase() === 'quit') {
-            continuePlaying = false;
-            break; // Exit the loop
-        }
-
-        const computerSelection = getComputerChoice();
-
-        console.log(`You chose ${playerSelection}`);
-        console.log(`Computer chose ${computerSelection}`);
-
-        const result = playRound(playerSelection, computerSelection);
-        console.log(result);
-
-        // Update scores as game progresses
-        if (result.includes('win')) {
-            playerScore++;
-        } else if (result.includes('lose')) {
-            computerScore++;
-        }
-    }
-}
-// To determine the winner after 5 rounds of gameplay
-if (playerScore > computerScore) {
-    console.log("Congratulations! You win!")
-} else if (playerScore < computerScore) {
-    console.log("You lose!")
-} else {
-    console.log("No winner, it's a tie!")
-}
-
-// game function is called to start the game 
-game();
